2f55d9f27fa01fc7e11c08643ddb47f6e03e0054
Ben Wichser <wichserpdx@gmail.com>
2019-12-10 15:38:39 -0800

    Day08Part2.  yay!

--- 2019Day08Part2.py
+++ 2019Day08Part2.py
@@ -2,12 +2,14 @@
 # Date: 12/10/2019
 # Description:  Image layer visualization.  see www.adventofcode.com/2019 (day 8) for more information
 
+
 def get_image_code(file_name):
     """Reads specified text file of single string and returns that string."""
     code_object = open(file_name)
     for line in code_object:
         code = str(line.strip())
     return code
+
 
 def image_layerer(code, width, height):
     """Accepts long string and returns it as a list of list of list of elements.  layers of rows of values."""
@@ -23,9 +25,12 @@ def image_layerer(code, width, height):
         image.append(image_layer)
     return image
 
-
-
-
+def layer_maker(top_layer, bottom_layer):
+    for row in range(len(top_layer)):
+        for column in range(len(top_layer[row])):
+            if top_layer[row][column] == 2:
+                top_layer[row][column] = bottom_layer[row][column]
+    return top_layer
 
 
 
@@ -33,16 +38,12 @@ image_width = 25
 image_height = 6
 layer_size = image_height*image_width
 
-code = get_image_code('/Users/benjamenwichser/Documents/OregonState/2019Fall/AdventOfCode/Advent_of_code_2019/mars_image_data.txt')
+code = get_image_code(
+    '/Users/benjamenwichser/Documents/OregonState/2019Fall/AdventOfCode/Advent_of_code_2019/mars_image_data.txt')
 image = image_layerer(code, image_width, image_height)
-max_count_list = [150 ,0, 0]
+current_layer  = [ [2 for _ in range(image_width)] for _ in range(image_height)]
 for layer in image:
-    count_list = code_counter(layer)
-    if count_list[0] < max_count_list[0]:
-        max_count_list = count_list
+    current_layer  = layer_maker(current_layer, layer)
 
-print( max_count_list[1] * max_count_list[2])
-
-
-
-
+for row in current_layer:
+    print(row)
